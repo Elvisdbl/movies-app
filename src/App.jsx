74d3c57ed2e13f1a07d665f1e7050441014ee956
Elvis Bonilla <danbony73@gmail.com>
2021-01-11 14:54:09 -0400

    I only managed to send the information of the search to the home page but I still haven't solved the problem of the query

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Detail from './components/Details/Details'
+// import { Container, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 
 function App() {
+
+  // const API_KEY = "96a1b3764e5654a82e0ee8f34ad97731";
+  // const API_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+  const [searchTerm, setSearchTerm] = useState([]);
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} exact/>
-        <Route path="/movie/:id" component={Detail}/>
-      </Switch>
-    </Router>
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a className="navbar-brand" href="/">The-movies</a>
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={searchTerm} onChange={handleOnChange} />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+
+      <Router>
+        <Switch>
+          {/* <Route path="/" component={() => <Home prop={'Hola'} />} exact /> */}
+          <Route
+            path='/'
+            render={() => (
+              <Home searchTerm={searchTerm} />
+            )} exact />
+          <Route path="/movie/:id" component={Detail} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,20 @@
 import Axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import Movie from '../../components/components/Movie';
-import { Container, Carousel, Navbar, Form ,FormControl } from 'react-bootstrap';
+import { Container, Carousel } from 'react-bootstrap';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
-function Home() {
+function Home(match) {
+    console.log(match);
+    let searchTerm = match.searchTerm;
+    console.log(searchTerm);
     const API_KEY = "96a1b3764e5654a82e0ee8f34ad97731";
     const API_IMG = "https://image.tmdb.org/t/p/original/";
     const API_POPULAR = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
-    const API_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
-
+    // const API_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
     const [nowPlaying, setNowPlaying] = useState([]);
     const [popular, setPopular] = useState([]);
-    const [searchTerm, setSearchTerm] = useState([]);
 
     const getNowPlaying = () => {
         Axios.get(`https://api.themoviedb.org/3/movie/now_playing`, {
@@ -23,7 +24,6 @@ function Home() {
         })
             .then(res => {
                 setNowPlaying(res.data.results);
-                console.log(res);
             })
             .catch(e => console.log(e));
     };
@@ -32,28 +32,17 @@ function Home() {
         Axios.get(API)
             .then(res => {
                 setPopular(res.data.results);
-                console.log(res);
             })
             .catch(e => console.log(e));
     };
 
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
-
-        if (searchTerm) {
-            getPopular(API_SEARCH + searchTerm);
-            setSearchTerm('');
-        }
-    };
-
-    const handleOnChange = (e) => {
-        setSearchTerm(e.target.value);
-    }
-
     useEffect(
         () => {
-            getNowPlaying();
-            getPopular(API_POPULAR); // This is the thing that i need to change
+            const Fetch = () => {
+                getNowPlaying();
+                getPopular(API_POPULAR); // This is the thing that i need to change
+            }
+            Fetch();
         }, [API_POPULAR]);
 
     const slider = nowPlaying.slice(0, 3).map(
@@ -70,16 +59,9 @@ function Home() {
         }
     );
 
+
     return (
         <Container>
-
-            <Navbar className="d-flex justify-content-between" bg="light" variant="light">
-                <Navbar.Brand href="/">TheMovies</Navbar.Brand>
-                <Form onSubmit={handleOnSubmit}>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" value={searchTerm} onChange={handleOnChange} />
-                </Form>
-            </Navbar>
-
             <Carousel
                 keyboard={true}
                 nextLabel=""
@@ -96,7 +78,6 @@ function Home() {
                     )
                 }
             </div>
-
         </Container>
     );
 }
